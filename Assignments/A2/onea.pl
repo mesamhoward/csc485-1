66bfc13ba749cf1a110bdf024f0aab638982cef5
Wen Xin Serena <wenxin6@teach.cs.toronto.edu>
2017-10-26 17:46:33 -0400

    1a works

--- Assignments/A2/onea.pl
+++ Assignments/A2/onea.pl
@@ -1,21 +1,24 @@
-%% Xin Serena Wen, wenxin6, 1000442666 %%
-
-bot sub [s, np, pp, vp].
+cat sub [s, npnom, vp, npacc, np, pp, n].          
 
 s sub [].
+npnom sub [].
+pronom sub [].
 
-vp sub [v npacc].
+vp sub [].          
 v sub [].
 
-pp sub [p npacc].
+npacc sub [].
+proacc sub [].
+
+pp sub [].
 p sub [].
 
-npnom sub [np, pronom].
-npacc sub [np, proacc].
-np sub [det n, det n pp, npl, npl pp].
-n sub [nsg, npl].
+np sub [].
+n sub [].
 nsg sub [].
 npl sub [].
+
+det sub [].
 
 she ---> pronom.
 fed ---> v.
@@ -27,5 +30,76 @@ with ---> p.
 
 srule rule
 s
-==>
-cat>
+===>
+cat> npnom,
+cat> vp.
+
+vprule rule
+vp
+===>
+cat> v,
+cat> npacc.
+
+pprule rule
+pp
+===>
+cat> p,
+cat> npacc.
+
+%% NPnom [2]
+npnom_nprule rule
+npnom
+===>
+cat> np.
+
+npnom_prorule rule
+npnom
+===>
+cat> pronom.
+
+%% NPacc [2]
+npacc_nprule rule
+npacc
+===>
+cat> np.
+
+npacc_prorule rule
+npacc
+===>
+cat> proacc.
+
+%% NP [4]
+np_detrule rule
+np
+===>
+cat> det,
+cat> n.
+
+np_det_pprule rule
+np
+===>
+cat> det,
+cat> n,
+cat> pp.
+
+np_nplrule rule
+np
+===>
+cat> npl.
+
+np_npl_pprule rule
+np
+===>
+cat> npl,
+cat> pp.
+
+%% N [2]
+np_sgrule rule
+n
+===>
+cat> nsg.
+
+np_plrule rule
+n
+===>
+cat> npl.
