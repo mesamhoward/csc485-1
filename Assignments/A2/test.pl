a0e95d76e58f5636effb01e6538b105f94b5baa1
Wen Xin Serena <wenxin6@teach.cs.toronto.edu>
2017-10-26 20:13:58 -0400

    bleh

--- Assignments/A2/test.pl
+++ Assignments/A2/test.pl
@@ -6,52 +6,33 @@ index sub [] intro [num:number].
 cat sub [s, np, vp, pp].
 s sub [].
 
-p sub [].
-pp sub [].
-
 det sub [].
 
 n sub [] intro [index:index].
 np sub [] intro [head:n].
 
+v sub [].
+
 %% Lexicon
 the ---> det.
 dog ---> (n,index:(num:sg)).
 puppies ---> (n,index:(num:pl)).
-with ---> p.
+fed ---> v.
 
 %%rules
 srule rule
 s
 ===>
-cat> (np, head:(index:(num:pl)).
-
-pprule rule
-pp
-===>
-cat> p,
-cat> np.
+cat> (np, head:(index:index),
+cat> v.
 
 nrule rule
 np
 ===>
 cat> n.
 
-n_pprule rule
-np
-===>
-cat> n,
-cat> pp.
-
 detrule rule
 np
 ===>
 cat> det,
 cat> n.
-
-det_pprule rule
-np
-===>
-cat> det,
-cat> n,
-cat> pp.
