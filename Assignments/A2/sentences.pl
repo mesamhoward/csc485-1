0a5f4f7be9db068e6bc10bc400e1653fcf3fc181
Serena Wen <wen.x.serena@hotmail.com>
2017-11-04 20:07:31 -0400

    Revert "Merge branch 'master' of https://github.com/xswen/csc485"
    This reverts commit 563badea1915f5f585f9e68cd8d970d564e813c1, reversing
    changes made to 7f20017945820d60b157f7f9132b650056e30696.

---
+++ Assignments/A2/sentences.pl
@@ -0,0 +1,5 @@
+% Xin Serena Wen, wenxin6, 1000442666
+test_sent([the, student, persuaded, the, teacher, to, sleep])
+test_sent([the, student, preferred, to, sleep])
+test_sent([the, student, promised, the, teacher, to, sleep])
+test_sent([the, student, expected, the, theacher, to, sleep])
