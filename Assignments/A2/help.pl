70294fbdbe99fab3e49f41ef57494825a2c09f4b
Serena Wen <wen.x.serena@hotmail.com>
2017-11-07 16:21:44 -0500

    fixing infclause

--- Assignments/A2/help.pl
+++ Assignments/A2/help.pl
@@ -53,7 +53,6 @@ bot sub [mood, tense, sem, cat, pos, verbal, nominal].
 			student sub [].
 			teacher sub [].
 		n_sem_or_none sub [n_sem, gap, none].
-			gap sub [].
 			none sub [].
 		theme_or_none sub [theme, none].
 
@@ -115,7 +114,7 @@ vtoinf rule
 (vp, vsem:(vform:fin, agent:Subj, exp:Subj))
 ===>
 cat> (v, vsem:(vform:fin, agent:Subj, theme:(theme, obj:Gap, action:InfC), ben:none, exp:none)),
-cat> inf_clause.
+cat> (inf_clause, vsem:InfC).
 %cat> (inf_clause, vsem:(InfC, vform:base, agent:Subj, theme:(theme, obj:none, action:Basev), ben:none, exp:Subj)).
 
 %inf_c->to+v
