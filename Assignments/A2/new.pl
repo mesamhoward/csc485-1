dfacdd31a553464ba88526b48f94a2da29bf9055
Wen Xin Serena <wenxin6@teach.cs.toronto.edu>
2017-11-06 22:02:29 -0500

    crap

--- Assignments/A2/new.pl
+++ Assignments/A2/new.pl
@@ -1,4 +1,4 @@
-:- ale_flag(subtypecover,_,off).
+:i- ale_flag(subtypecover,_,off).
 :- discontiguous sub/2,intro/2.
 
 bot sub [mood, tense, sem, cat, pos, verbal, nominal].
@@ -18,8 +18,8 @@ bot sub [mood, tense, sem, cat, pos, verbal, nominal].
 			vp intro [mood:indicative].
 		np sub [].
 
-        verbal sub [v,vproj] intro [vsem:n_sem].
-        nominal sub [n,np] intro [nsem:v_sem].
+        verbal sub [v,vproj] intro [vsem:v_sem].
+        nominal sub [n,np] intro [nsem:n_sem].
 	
 	% mood and tense for verbs
 	tense sub [past, present].
@@ -27,14 +27,17 @@ bot sub [mood, tense, sem, cat, pos, verbal, nominal].
 		present sub [].
         mood sub [indicative,infinitive].
                 indicative intro [tense:tense].
-                infinitive sub [].
+                infinitive sub [] intro [tense: present].
 
 	% semantics for verbs and nouns
 	sem sub [v_sem, n_sem].
 
 		% semantics for verbs
 		v_sem sub [prefer, persuade, promise, expect, sleep]
-		      intro [].   % This should not be empty!  Fill in features for this and
+		      intro [vform:vform, tense:tense].   % This should not be empty!  Fill in features for this and
+			vform sub [fin, base].
+				fin sub [].
+				base sub [].
                                   %  the following subtypes:
 			prefer sub [].
 			persuade sub [].
@@ -43,6 +46,44 @@ bot sub [mood, tense, sem, cat, pos, verbal, nominal].
 			sleep sub [].
 
 		% semantics for nouns
-		n_sem sub [student, teacher].
+		n_sem sub [student, teacher] intro [amt:amount].
+			amount sub [sg, pl].
 			student sub [].
 			teacher sub [].
+
+%Lexicon
+the ---> det.
+
+student ---> (n, nsem:(student, amt:sg)).
+students ---> (n, nsem:(student, amt:pl)).
+
+sleep ---> (v, vsem:(sleep, vform:base)).
+slept ---> (v, vsem:(sleep, vform:fin, tense:tense)).
+
+%Rules
+
+%S->NP+VP
+srule rule
+(s, mood:(indicative, tense:past))
+===>
+cat> np,
+cat> ((vp, mood:(indicative, tense:past)), vform:fin).
+
+%NP->det+n
+detrule rule
+np
+===>
+cat> det,
+cat> n.
+
+%NP->N
+nrule rule
+np
+===>
+cat> (n, nsem:(Nsem, amt:pl)).
+
+%VP->V
+ivrule rule
+((vp, mood:(Mood, tense:Tense)), vform:Vform)
+===>
+cat> (v, vsem:(vsem, vform:Vform, tense:Tense)).
