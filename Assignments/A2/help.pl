af976cd6d4c052ffb898fcb3cd7b649106ce96b1
Serena Wen <wen.x.serena@hotmail.com>
2017-11-07 20:48:58 -0500

    toinf

--- Assignments/A2/help.pl
+++ Assignments/A2/help.pl
@@ -51,7 +51,7 @@ bot sub [mood, tense, sem, cat, pos, verbal, nominal].
 			sleep sub [] intro [vform:verbform, agent:none, theme:none, ben:none, exp:n_sem].
 
 		% semantics for nouns
-		n_sem sub [student, teacher] intro [amt:amount].
+		n_sem sub [student, teacher, gap] intro [amt:amount].
 			amount sub [sg, pl].
 			student sub [].
 			teacher sub [].
@@ -113,16 +113,17 @@ cat> (v, vsem:(vform:fin, agent:none, exp:Subj)).
 vtoinf rule
 (vp, vsem:(Vpast, vform:fin, agent:Subj, exp:Subj))
 ===>
-cat> (v, vsem:(vform:fin, agent:Subj, theme:(theme, obj:Gap, action:Lol), ben:none, exp:none)),
-cat> (inf_clause, vsem:(agent:Subj, exp:Subj)).
+cat> (v, vsem:(vform:fin, agent:Subj, theme:(theme, obj:gap, action:Infsem), ben:none, exp:none)),
+cat> (inf_clause, vsem:(Infsem, agent:Subj, exp:Subj)).
 %cat> (inf_clause, vsem:(InfC, vform:base, agent:Subj, theme:(theme, obj:none, action:Basev), ben:none, exp:Subj)).
 
 %inf_c->to+v
+%Bsem=base sem
 infrule rule
-(inf_clause, vsem:(agent:Subj, exp:Subj))
+(inf_clause, vsem:(Bsem, agent:Subj, exp:Subj))
 ===>
 cat> toinf,
-cat> (v, vsem:(vform:base, agent:none, theme:none, ben:none, exp:Subj)).
+cat> (v, vsem:(Bsem, vform:base, agent:none, theme:none, ben:none, exp:Subj)).
 
 %VP->V+NP+inf_clause
 %v_np_infcrule rule
