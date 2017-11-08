7308ebef51b41f815e8674b699aa81d8ecb8701c
Serena Wen <wen.x.serena@hotmail.com>
2017-11-08 13:53:59 -0500

    changed some files

--- Assignments/A2/sentences.pl
+++ Assignments/A2/sentences.pl
@@ -5,8 +5,13 @@ test_sent([the, student, persuaded, the, teacher, to, sleep]).
 test_sent([the, student, preferred, to, sleep]).
 test_sent([the, student, promised, the, teacher, to, sleep]).
 test_sent([the, student, expected, the, teacher, to, sleep]).
+test_sent([students, expected, to, expect, to, expect, to, promise, the, students, to, sleep]).
 
 %%Fail
 test_sent([students, expected, to, sleep, to, sleep], fails).
 test_sent([the, student, persuaded.], fails).
-test_sent((the, student, persuaded, to, sleep), fails).
+test_sent([the, student, persuaded, to, sleep], fails).
+test_sent([the, student, expected], fails).
+test_sent([the, student, expected, the, teacher], fails).
+test_sent([the, student, expected, to], fails).
+test_sent([student, expected, to, sleep], fails).
