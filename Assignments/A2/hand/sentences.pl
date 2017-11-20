20a4280a11b8ca32f94f4a2476fba44fe2678a3c
Serena Wen <wen.x.serena@hotmail.com>
2017-11-20 15:05:54 -0500

    fuck

--- Assignments/A2/hand/sentences.pl
+++ Assignments/A2/hand/sentences.pl
@@ -4,12 +4,12 @@
 test_sent([the, student, persuaded, the, teacher, to, sleep]).
 test_sent([the, student, preferred, to, sleep]).
 test_sent([the, student, promised, the, teacher, to, sleep]).
-test_sent([the, student, expected, the, teacher, to, sleep]).
+test_sent([teachers, expected, the, teacher, to, sleep]).
 test_sent([students, expected, to, expect, to, expect, to, promise, the, students, to, sleep]).
 
 %%Fail
 test_sent([students, expected, to, sleep, to, sleep], fails).
-test_sent([the, student, persuaded.], fails).
+test_sent([the, student, persuaded], fails).
 test_sent([the, student, persuaded, to, sleep], fails).
 test_sent([the, student, expected], fails).
 test_sent([the, student, expected, the, teacher], fails).
